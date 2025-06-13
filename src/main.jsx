a5b4af5290998e76914efdd4e7318dd23cbcfa0e
Tanishagupta17 <123789264+Tanishagupta17@users.noreply.github.com>
2025-06-13 20:11:19 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {Provider} from 'react-redux'
 
 import './index.css';
 import App from './App';
-import { store } from './State/Store';
+import { store } from './State/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
